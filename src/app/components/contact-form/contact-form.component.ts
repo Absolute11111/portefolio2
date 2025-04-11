cc0d4f67211947008254cf6235326a277f59bd87
Sebastian <ibibis.yourname@gmail.com>
2025-04-11 09:02:15 +0200

    mailing ok style modifier

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from 'emailjs-com';
 
 @Component({
-  standalone: true,
   selector: 'app-contact-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+  formData = {
+    name: '',
+    email: '',
+    message: '',
+    honeypot: '' // champ invisible
+  };
 
+  sendEmail() {
+    if (this.formData.honeypot) {
+      console.warn('Spam détecté, email non envoyé.');
+      return; // stop si le champ est rempli (robot)
+    }
+
+    const serviceID = 'service_dtcbuun';
+    const templateID = 'template_mpizpcs';
+    const userID = 'NuEdGb0hedJjsfFEh';
+
+    emailjs.send(serviceID, templateID, this.formData, userID)
+      .then(() => {
+        alert('✅ Email bien envoyé !');
+        this.formData = { name: '', email: '', message: '', honeypot: '' }; // reset form
+      }, (error) => {
+        console.error('Erreur EmailJS:', error);
+        alert('❌ Une erreur est survenue.');
+      });
+  }
 }
